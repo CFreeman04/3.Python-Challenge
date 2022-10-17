df5eca6a3773d49958cb1e52937efaea3c446cb9
CFreeman04 <charlie.e.freeman@gmail.com>
2022-10-17 18:15:57 -0500

    Final Python

--- Instructions/PyBank/import os.py
+++ Instructions/PyBank/import os.py
@@ -7,6 +7,7 @@ csvpath = os.path.join('Resources', 'budget_data.csv')
 totalmonths = []
 profits = []
 pc = []
+
 # Method 2 using CVS Module
 with open(csvpath) as csvfile:
 
@@ -14,7 +15,7 @@ with open(csvpath) as csvfile:
     print(csvreader)
 
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    #print(f"CSV Header: {csv_header}")
 
     for row in csvreader:
         totalmonths.append(row[0])
@@ -22,9 +23,16 @@ with open(csvpath) as csvfile:
 
 for i in range(len(profits) - 1):
     pc.append(profits[i+1] - profits[i])
-    
-print(totalmonths[1])
-print(totalmonths.index("Jan-10"))
+
+
+print(len(totalmonths))
+print(sum(profits))
+print(sum(pc) / len(pc))
+print(totalmonths[pc.index(max(pc))+1])
+print(totalmonths[pc.index(min(pc))+1])
+print(min(pc))
+
+#print(totalmonths.index("Jan-10"))
 
 
 
